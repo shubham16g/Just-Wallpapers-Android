86ec4b9a72dd1aba72ffbfa815db99080ef7184c
Shubham Gupta <shubham.developer16@gmail.com>
2022-03-04 13:07:01 +0530

    Fixed Boucy List effect in List Page

--- app/src/main/java/com/shubhamgupta16/wallpaperapp/adapters/ViewPager2Adapter.kt
+++ app/src/main/java/com/shubhamgupta16/wallpaperapp/adapters/ViewPager2Adapter.kt
@@ -1,9 +1,13 @@
 package com.shubhamgupta16.wallpaperapp.adapters
 
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentViewHolder
 import com.shubhamgupta16.wallpaperapp.ui.components.VerticalWallpapersFragment
 
 class ViewPager2Adapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
@@ -20,6 +24,16 @@ class ViewPager2Adapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) 
 
     override fun createFragment(position: Int): Fragment {
         return VerticalWallpapersFragment.getInstance(category = mFragmentList[position])
+    }
+
+    override fun onBindViewHolder(
+        holder: FragmentViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        super.onBindViewHolder(holder, position, payloads)
+        Log.d("Adapter", "onBindViewHolder: $position")
+        Log.d("Adapter", "onBindViewHolder payloads: $payloads")
     }
 
     override fun getItemCount(): Int {
